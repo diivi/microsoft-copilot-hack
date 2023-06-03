34a32412a30350349d3c30962b5f33e8b24fd46d
TheLazron <kaushik.rohan31@gmail.com>
2023-06-03 15:36:11 +0530

    transactions add card

--- apis/finance-tracker/src/schemas/cardSchemas.ts
+++ apis/finance-tracker/src/schemas/cardSchemas.ts
@@ -3,8 +3,8 @@ import { z } from "zod";
 //create card schema
 const createCardSchema = z.object({
   cardLabel: z.string().min(1).max(20),
-  cap: z.number().min(1).max(10),
-  cardNumber: z.number().min(1).max(20),
+  cap: z.number().min(1).max(1000000),
+  cardNumber: z.number().min(1).max(1000000),
   bank: z.string().min(1).max(20),
 });
 
